3c8a7ac84872fcc5dd74511fa620f3fdcade75ea
Manasi Dayete <manasidayete@gmail.com>
2026-02-11 17:47:32 +0530

    new pipe folder

---
+++ PracticeGIT/src/app/Pipe/pipe-demo/pipe-demo.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pipe-demo',
+  imports: [],
+  templateUrl: './pipe-demo.html',
+  styleUrl: './pipe-demo.css',
+})
+export class PipeDemo {
+
+}
